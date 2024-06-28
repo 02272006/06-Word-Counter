02ff5b30c1776a8106c848484db100091a5b3f67
Musfira waseem <kwasim7844>
2024-06-28 07:38:30 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// INSTALLATION
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.yellow.bold(`WELCOME TO WORD COUNTER!`));
+const ans = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: chalk.blue.italic("Please write a sentence and I'll count the words!"),
+});
+const word = ans.sentence.trim().split(" ");
+console.log(chalk.blue.italic(`The words counted by Word counter are: ${word.length}`));
